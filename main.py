8a7774a0c8d2237968700acfe98c64b0a4f9621d
SanguiBibe <vasilenko.va@phystech.edu>
2025-03-07 01:44:16 +0300

    gitignore added, get token from .env added, data storage added, plotting added and some small changes

--- main.py
+++ main.py
@@ -4,18 +4,33 @@ import re
 import unicodedata
 import json
 import aiohttp
-from telegram import Update
+from telegram import Update, InputFile
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler, CallbackContext, JobQueue
 import ssl
 import certifi
 import asyncio
 import logging
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import io
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
 
 ASK_TICKERS = 1
-TOKEN = '7860372015:AAE7RF8YoLcLSAH0Q2C0JCDHTWsxF2-Yzjk'
+TOKEN = os.getenv("API_KEY")
+
+if TOKEN:
+    print("Token found")
+else:
+    print("Token not found. Please set the TOKEN .env variable.")
 
 user_data = {}
-ticker_idx = {}
  
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -35,6 +50,39 @@ def binarySearch(arr, target):
             r = mid-1
     return -1  
 
+def writeCSV(new_data, max_rows):
+    # Define column names
+    columns = ['Ticker', 'Price', 'Datetime']
+    
+    # Convert new_data to a DataFrame
+    new_row = pd.DataFrame([new_data], columns=columns)
+    
+    try:
+        # Load existing data
+        df = pd.read_csv("data.csv")
+    except FileNotFoundError:
+        # If the file doesn't exist, create a new DataFrame
+        df = pd.DataFrame(columns=columns)
+    
+    # Append new row
+    if df.empty:
+        df = new_row  # If df is empty, directly assign new_row
+    else:
+        df = pd.concat([df, new_row], ignore_index=True)
+    
+    # Remove oldest rows if the maximum size is exceeded
+    if len(df) > max_rows:
+        df = df.tail(max_rows)  # Keep only the last max_rows rows
+    
+    # Save the updated DataFrame to CSV
+    df.to_csv("data.csv", index=False)
+
+async def errorHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a message to the user."""
+    print(f"Error occurred: {context.error}")
+    if update and update.message:
+        await update.message.reply_text("An error occurred. Please try again later.")
+
 async def price(ticker: str, update: Update):
     ticker = ticker if ticker else update.message.text.strip().upper()
     url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json'
@@ -44,10 +92,7 @@ async def price(ticker: str, update: Update):
         await update.message.reply_text("Failed to fetch data from the server.")
         return
     data = response.json()
-
-    if ticker not in ticker_idx:
-        ticker_idx[ticker] = binarySearch(data["marketdata"]["data"], ticker)
-    idx = ticker_idx.get(ticker, -1)
+    idx = binarySearch(data["marketdata"]["data"], ticker)
 
     if idx != -1:
         ticker_info = data["marketdata"]["data"][idx]
@@ -86,6 +131,11 @@ async def pricePrompt(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text("Please enter the ticker whose price you want to find out:")
     return ASK_TICKERS
 
+async def plotPrompt(update: Update, context: CallbackContext) -> int:
+    context.user_data["action"] = "plot"
+    await update.message.reply_text("Please enter tracked tickers whose plot you want to look at:")
+    return ASK_TICKERS
+
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     if user_id not in user_data:
@@ -97,6 +147,9 @@ async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     tickers.update(newTickers)  
 
     # Access the tickers correctly using "tickers" instead of ["tickers"]
+    if "job_started" not in context.user_data:
+        context.job_queue.run_repeating(periodCheck, interval=300, first=60, data=update, user_id=update.message.from_user.id)
+        context.user_data["job_started"] = True
     await update.message.reply_text(f"Your updated tickers are: {', '.join(tickers)}")
 
     return ConversationHandler.END  # End the conversation
@@ -144,10 +197,7 @@ async def getPrice(ticker: str, update: Update):
         await update.message.reply_text("Failed to fetch data from the server.")
         return
     data = response.json()
-
-    if ticker not in ticker_idx:
-        ticker_idx[ticker] = binarySearch(data["marketdata"]["data"], ticker)
-    idx = ticker_idx.get(ticker, -1)
+    idx = binarySearch(data["marketdata"]["data"], ticker)
 
     if idx != -1:
         ticker_info = data["marketdata"]["data"][idx]
@@ -156,11 +206,12 @@ async def getPrice(ticker: str, update: Update):
     
     try:
         price = ticker_info[2]
+        datetime = ticker_info[47]
         currency = "RUB"
         ticker = ticker_info[0]
 
         if price is not None:
-            return price
+            return price, datetime[:-3]
     except IndexError:
         await update.message.reply_text("Error: Data for ticker is incomplete or invalid.")
 
@@ -171,24 +222,26 @@ async def periodCheck(context: ContextTypes.DEFAULT_TYPE):
         user_data[user_id] = {}
 
     if user_id and user_data[user_id].get("tickers", -1) != -1:
+        max_rows = (7*24*60)//5 * len(user_data[user_id]["tickers"])
+        prices = user_data[user_id].setdefault("prices", dict())
         for ticker in user_data[user_id]["tickers"]:
-            prices = user_data[user_id].setdefault("prices", dict())
-            curr_price = await getPrice(ticker, update)
+            curr_price, datetime = await getPrice(ticker, update)
             prev_price = prices.setdefault(ticker, curr_price)
             
             if prev_price and curr_price:
+                writeCSV([ticker, curr_price, datetime], max_rows)
+
                 change = round(prev_price / curr_price, 3)
                 if change >= 1.010 or change <= 0.990:
                     await context.bot.send_message(user_id, text=f"{ticker} price changed from {prev_price} to " + 
                     f"{curr_price} ({round(100*abs(change-1), 3)}%) in 5 minutes. Check it!") 
 
-            user_data[user_id]["prices"][ticker] = curr_price
+            user_data[user_id]["prices"][ticker] = curr_price if curr_price is not None else prev_price
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello! I am your FineTrackBot. I'll help you to track some data about your stocks "
                                     "and inform you if some of stocks have increased their prices significantly during "
                                     "small period of time. Just let me know, which tickers you are interested in.")
-    context.job_queue.run_repeating(periodCheck, interval=300, first=60, data=update, user_id=update.message.from_user.id)
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text("Operation cancelled.")
@@ -203,23 +256,73 @@ async def handleTickers(update: Update, context: CallbackContext) -> int:
         await delete(update, context)
     elif action == "price":
         await price(None, update)
+    elif action == "plot":
+        await plot(update, context)
     else:
         await update.message.reply_text("Unknown action.")
     
     return ConversationHandler.END  # End the conversation
 
-# Main function to run the bot
+async def plot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+    if user_id not in user_data:
+        user_data[user_id] = {}
+
+    tickers = set(ticker.strip().upper() for ticker in update.message.text.split(","))
+    if tickers:
+        df = pd.read_csv("data.csv")
+        df['Datetime'] = pd.to_datetime(df["Datetime"], format='%Y-%m-%d %H:%M')
+
+        df_sorted = df.sort_values(by=['Ticker', 'Datetime'])
+        grouped = df_sorted.groupby('Ticker')
+
+        fig, axs = plt.subplots(len(tickers), figsize=(24,30))   
+
+        counter = 0
+        for ticker, group in grouped:
+            if ticker in tickers:
+                x = group["Datetime"][:2016]
+                y = group["Price"][:2016]
+
+                if len(tickers) > 1:
+                    axs[counter].plot(x, y, label=f"{ticker}")
+                    axs[counter].set_xlabel("Datetime", fontsize=24)
+                    axs[counter].set_ylabel(r"Price", fontsize=24)
+                    axs[counter].tick_params(axis='both', labelsize=24) 
+                    axs[counter].legend(loc='best', fontsize=30)
+                    axs[counter].grid()
+                else:
+                    axs.plot(x, y, label=f"{ticker}")
+                    axs.set_xlabel("Datetime", fontsize=24)
+                    axs.set_ylabel(r"Price", fontsize=24)
+                    axs.tick_params(axis='both', labelsize=24) 
+                    axs.legend(loc='best', fontsize=30)
+                    axs.grid()
+                plt.tight_layout()
+                counter += 1
+        
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+
+        await context.bot.send_photo(user_id, photo=InputFile(buf))
+
+        buf.close()
+
+    else:
+        await update.message.reply_text("You have no tracked tickers.")
+    
+
 def main():
-    # Replace 'YOUR_TOKEN' with the token you got from BotFather
     application = Application.builder().token(TOKEN).build()
 
 
-        # Define the conversation handler
     conversation_handler = ConversationHandler(
         entry_points=[
             CommandHandler("add", addPrompt), 
             CommandHandler("delete", deletePrompt),
-            CommandHandler("price", pricePrompt)
+            CommandHandler("price", pricePrompt),
+            CommandHandler("plot", plotPrompt)
         ],  # Command to start the conversation
         states={
             ASK_TICKERS: [MessageHandler(filters.TEXT & ~filters.COMMAND, handleTickers)],  # State to capture tickers
@@ -234,8 +337,9 @@ def main():
     application.add_handler(CommandHandler("clear", clear))
     application.add_handler(conversation_handler)
 
+    application.add_error_handler(errorHandler)
+
     # Start the bot
     application.run_polling()
 
 main()
-
